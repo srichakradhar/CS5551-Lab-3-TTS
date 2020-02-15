54de9f1204ecd855737deddea96a408eeebb01c1
srichakradhar <srichakra3nsr3@gmail.com>
2020-02-14 21:20:45 -0600

    add login signup tss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { TtsComponent } from './tts/tts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    TtsComponent
   ],
   imports: [
     BrowserModule,
